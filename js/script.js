a27eed5dd47c5949f5997453f8aac466337b6521
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-29 17:30:11 +0200

    esercizio completo pari e dispari

--- js/script.js
+++ js/script.js
@@ -2,25 +2,24 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
-// function palWord(sentence){
-//     let result = ``;
-//     for(let i = sentence.length-1;i>= 0; i--){
-//     result += sentence[i];
-//     }
+function palWord(sentence){
+    let result = ``;
+    for(let i = sentence.length-1;i>= 0; i--){
+    result += sentence[i];
+    }
     
-// console.log(sentence);
-// console.log(result);
-// if(sentence === result){
-//     console.log(`la parola è palindroma`);
-// }
-// else{
-//     console.log(`la parola non è palindroma`);
-// }
-//     return result;
-// }
+console.log(sentence);
+console.log(result);
+if(sentence === result){
+    console.log(`la parola è palindroma`);
+}
+else{
+    console.log(`la parola non è palindroma`);
+}
+    return result;
+}
 
-// const palword = palWord(prompt(`scrivi una parola e io ti diro se questa è palindroma o no:`));
-
+const palword = palWord(prompt(`scrivi una parola e io ti diro se questa è palindroma o no:`));
 
 // esercizio 2:
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -29,25 +28,7 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-/*let humanPlayer = Math.round(Math.random()*5 + 1);
- let cpuPlayer = Math.round(Math.random()*5 + 1);
- console.log(humanPlayer);
- console.log(cpuPlayer);
-
-
-if(humanPlayer > cpuPlayer){
-    console.log(`eh ci devi ancora arrivare all uomo macchina!!`);
-}
-else if(humanPlayer === cpuPlayer){
-    console.log(`incredibile pareggio!!`)
-}
-else{
-    console.log(`terminator è vicino!!`);
-}*/
-
-
-
-let numberUser = prompt(`scegli un numero da 1 a 5:`);
+let numberUser = parseInt(prompt(`scegli un numero da 1 a 5:`));
 console.log(numberUser);
 
 if(numberUser % 2 == 0){
@@ -62,5 +43,29 @@ function randomCpunumber(min=1 , max=5){
 }
 const cpuNumber = randomCpunumber();
 console.log(`il numero scelto per il computer sara:`+ cpuNumber);
+
+const sum = numberUser + cpuNumber;
+console.log(sum);
+
+function isEven(){
+    let total = false;
+    if(sum % 2 === 0){
+        total = true;
+        console.log(`la somma dei due numeri e pari!`)
+    }
+    else{console.log(`la somma dei numeri è dispari!`)}
+    return total;
+}
+    const somma = isEven();
+
+    if(cpuNumber > numberUser){
+        console.log(`Ha vinto il computer!`);
+    }
+    else if(cpuNumber === numberUser){
+        console.log(`incredibile pareggio!!`)
+    }
+    else{
+        console.log(`bravo hai vinto!!`);
+    }
 
 
