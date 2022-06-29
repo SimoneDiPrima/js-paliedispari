ab76614b209e034ee2941ac01e6c488741b1228a
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-29 15:38:31 +0200

    esercizio parola palindroma completo

--- js/script.js
+++ js/script.js
@@ -2,12 +2,21 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
+function palWord(sentence){
+    let result = ``;
+    for(let i = sentence.length-1;i>= 0; i--){
+    result += sentence[i];
+    }
+    
+console.log(sentence);
+console.log(result);
+if(sentence === result){
+    console.log(`la parola è palindroma`);
+}
+else{
+    console.log(`la parola non è palindroma`);
+}
+    return result;
+}
 
-
-
-
-
-
-
-const word = prompt(`scrivi una parola e io ti diro se questa è palindroma o no`);
-console.log(word);
+const palword = palWord(prompt(`scrivi una parola e io ti diro se questa è palindroma o no:`));
