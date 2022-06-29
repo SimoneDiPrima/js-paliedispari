3502f22b18078ecd5b63b34e1ae564177946c619
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-29 14:46:57 +0200

    primo punto analisi script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log(`js ok`);
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+
+
+
+
+
+
+
+const word = prompt(`scrivi una parola e io ti diro se questa è palindroma o no`);
+console.log(word);
